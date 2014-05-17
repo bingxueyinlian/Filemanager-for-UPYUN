d7cafde55430b01781213d2a771984885fd2832a
bingxueyinlian <290388220@qq.com>
2014-05-17 11:33:19 +0800

    SignedSigned-off-by: bingxueyinlian <290388220@qq.com> Signed-off-by: bingxueyinlian <290388220@qq.com>

---
+++ Filemanager-for-UPYUN/FileType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Filemanager_for_UPYUN
+{
+    /// <summary>
+    /// 文件类型
+    /// </summary>
+    class FileType
+    {
+        /// <summary>
+        /// 目录:F
+        /// </summary>
+        public const string Dir = "F";
+        /// <summary>
+        /// 文件:N
+        /// </summary>
+        public const string File = "N";
+
+    }
+}
